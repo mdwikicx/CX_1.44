d8ee5cb3f465271f5d147c4f8dcb80329e042b53
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-11 03:46:56 +0300

    1

--- ContentTranslation/modules/mw.cx.init.js
+++ ContentTranslation/modules/mw.cx.init.js
@@ -59,6 +59,9 @@
 		services.requestManager = new mw.cx.MwApiRequestManager(mw.cx.sourceLanguage, mw.cx.targetLanguage, services.siteMapper);
 		services.MTService = new mw.cx.MachineTranslationService(mw.cx.sourceLanguage, mw.cx.targetLanguage, services.siteMapper);
 		services.MTManager = new mw.cx.MachineTranslationManager(mw.cx.sourceLanguage, mw.cx.targetLanguage, services.MTService);
+		// By: Ibrahem Qasim
+		services.campaign = query.campaign;
+		services.tr_type = query.tr_type;
 
 		getTargetTitle(query.targettitle, sourceTitle, services.MTService).then((targetTitle) => {
 			const sourceWikiPage = new mw.cx.dm.WikiPage(sourceTitle, mw.cx.sourceLanguage, sourceRevision, sourceSectionTitle);
